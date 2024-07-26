c69865e9bbca693b5ed2f0354eeab0724c779c48
Oussama Chihaoui <chihaouioussama@gmail.com>
2024-07-26 19:14:23 +0100

    added Profile component

--- frontend/src/Pages/User/Profile.jsx
+++ frontend/src/Pages/User/Profile.jsx
@@ -1,7 +1,144 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { userInfo } = useSelector((state) => state.auth);
+  //consts
+  const dispatch = useDispatch();
+
+  return (
+    <div className="bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931]">
+      <aside className="hidden py-4 md:w-1/3 lg:w-1/4 md:block">
+        <div className="sticky flex flex-col gap-2 p-4 text-sm border-r border-indigo-100 top-12">
+          <h2 className="pl-3 mb-4 text-2xl font-semibold">Settings</h2>
+          <a
+            href="#"
+            className="flex items-center px-3 py-2.5 font-bold bg-white  text-indigo-900 border rounded-full"
+          >
+            Pubic Profile
+          </a>
+        </div>
+      </aside>
+      <form className="w-full min-h-screen py-1 md:w-2/3 lg:w-3/4">
+        <div className="p-2 md:p-4">
+          <div className="w-full px-6 pb-8 mt-8 sm:max-w-xl sm:rounded-lg">
+            <h2 className="pl-6 text-2xl font-bold sm:text-xl">
+              Public Profile
+            </h2>
+            <div className="grid max-w-2xl mx-auto mt-8">
+              <div className="flex flex-col items-center justify-around  sm:flex-row sm:space-y-0">
+                <img
+                  className="object-cover w-40 h-40 p-1 rounded-full ring-2 ring-gray-600"
+                  src={userInfo.photo}
+                  alt="Bordered avatar"
+                />
+                <div className="flex flex-col space-y-5 sm:ml-8 ">
+                  <input
+                    type="file"
+                    className="py-3.5 px-7 text-base font-medium  focus:outline-none  rounded-lg   focus:z-10 focus:ring-4 focus:ring-indigo-200 file:mr-4 file:py-2 file:px-4  file:rounded-full file:border-0
+                     file:text-sm file:font-semibold  file:bg-violet-50 file:text-violet-700  hover:file:bg-violet-100 "
+                  />
+                </div>
+              </div>
+              <div className="items-center mt-8 sm:mt-14 text-[#202142]">
+                <div className="flex flex-col items-center w-full mb-2 space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 sm:mb-6">
+                  <div className="w-full">
+                    <label
+                      htmlFor="first_name"
+                      className="block mb-2 text-sm font-medium text-indigo-900 "
+                    >
+                      Your first name
+                    </label>
+                    <input
+                      type="text"
+                      id="first_name"
+                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
+                      placeholder="Your first name"
+                      defaultValue={userInfo.firstName}
+                      name="firstName"
+                    />
+                  </div>
+                  <div className="w-full">
+                    <label
+                      htmlFor="last_name"
+                      className="block mb-2 text-sm font-medium text-indigo-900 "
+                    >
+                      Your last name
+                    </label>
+                    <input
+                      type="text"
+                      id="last_name"
+                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
+                      placeholder="Your last name"
+                      defaultValue={userInfo.lastName}
+                      name="lastName"
+                    />
+                  </div>
+                </div>
+                <div className="mb-2 sm:mb-6">
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-indigo-900 "
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
+                    placeholder="your.email@mail.com"
+                    name="email"
+                    defaultValue={userInfo.email}
+                  />
+                </div>
+                <div className="flex flex-col items-center w-full mb-2 space-x-0 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0 sm:mb-6">
+                  <div className="w-full">
+                    <label
+                      htmlFor="first_name"
+                      className="block mb-2 text-sm font-medium text-indigo-900 "
+                    >
+                      Your password
+                    </label>
+                    <input
+                      type="text"
+                      id="first_name"
+                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
+                      placeholder="Your first name"
+                      name="password"
+                    />
+                  </div>
+                  <div className="w-full">
+                    <label
+                      htmlFor="last_name"
+                      className="block mb-2 text-sm font-medium text-indigo-900 "
+                    >
+                      Confirm your password
+                    </label>
+                    <input
+                      type="text"
+                      id="last_name"
+                      className="bg-indigo-50 border border-indigo-300 text-indigo-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 "
+                      placeholder="Your last name"
+                      name="confirmPassword"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex justify-end">
+                  <button
+                    type="submit"
+                    className="text-white bg-indigo-700  hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
