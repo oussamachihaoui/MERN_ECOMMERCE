38bc810260938e4d920e2e08a6e859dc511b4c3c
Oussama Chihaoui <chihaouioussama@gmail.com>
2024-07-30 11:04:14 +0100

    added createCatagory controller

--- backend/controllers/catagoryController.js
+++ backend/controllers/catagoryController.js
@@ -1,8 +1,31 @@
 import expressAsyncHandler from "express-async-handler";
+import Catagory from "../Models/catagoryModel.js";
 
 // create catagory
 const createCatagory = expressAsyncHandler(async (req, res) => {
-  res.send("CREATE CATAGORY");
+  const { name } = req.body;
+  const existsCatagory = await Catagory.findOne({ name });
+
+  if (!name) {
+    res.status(400).json({ message: "name is required" });
+    return;
+  }
+
+  if (existsCatagory) {
+    res.status(400).json({
+      message: "Catagory already exists",
+    });
+    return;
+  }
+  const catagory = await Catagory.create({ name });
+
+  if (catagory) {
+    res.status(201).json(catagory);
+  } else {
+    res.status(400).json({
+      message: "try again...",
+    });
+  }
 });
 
 // get a catagory
